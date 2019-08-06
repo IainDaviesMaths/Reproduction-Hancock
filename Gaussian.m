6440fc1e916c1e3649f1c0a3a2a9e4a9a0dd11b1
Iain Davies <id318@tesla.damtp.cam.ac.uk>
2019-08-06 16:35:00 +0100

    Code to produce Figure 5 and 6

--- Gaussian.m
+++ Gaussian.m
@@ -2,13 +2,15 @@ function [ array ] = Gaussian( size )
 %GAUSSIAN creates a size by size array with gaussian values centred in the
 %middle.
 
+sigma=size*5/32;
+
 array=zeros(size,size);
 x0=(size/2)+0.5;
 y0=x0;
 
 for x=1:size
     for y=1:size
-        array(x,y)=exp(-((x-x0)^2+(y-y0)^2)/(2*100));
+        array(x,y)=exp(-((x-x0)^2+(y-y0)^2)/(2*sigma^2));
     end
 end
 
